b79f315fcb49c0c67127dd1ff9f63a5236c3377b
Augustine Ubong <ubongclouds@gmail.com>
2024-08-22 12:09:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for contacting us, we will get back to you shortly.");
+  });
